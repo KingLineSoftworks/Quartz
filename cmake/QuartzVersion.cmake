e028c3c0a93357d02c73b8ce4ac3c5b90502d8bd
Kejoko <keeganjkochis@gmail.com>
2024-06-13 17:30:54 -0600

    Update cmake version

--- cmake/QuartzVersion.cmake
+++ cmake/QuartzVersion.cmake
@@ -10,6 +10,10 @@ endfunction()
 
 # -----=====***** the versions *****=====----- #
 
+##### Major Version 0 #####
+
+# Minor Version 1 - Fundamental Rendering
+
 #set_quartz_major_minor_patch_versions(0 0 0) # initialize project and get vulkan working
 #set_quartz_major_minor_patch_versions(0 1 0) # load 3d models
 #set_quartz_major_minor_patch_versions(0 1 1) # 3d camera movement
@@ -17,4 +21,5 @@ endfunction()
 #set_quartz_major_minor_patch_versions(0 1 3) # multiple gltf models
 #set_quartz_major_minor_patch_versions(0 1 4) # default textures, materials, normal textures, emission textures, occlusion textures
 #set_quartz_major_minor_patch_versions(0 1 5) # metallic roughness PBR BRDF with point lights and spot lights
-set_quartz_major_minor_patch_versions(0 1 6) # skyboxes =D so preeetttttttty (and ability to create multiple pipelines)
+#set_quartz_major_minor_patch_versions(0 1 6) # skyboxes =D so preeetttttttty (and ability to create multiple pipelines)
+set_quartz_major_minor_patch_versions(0 1 7) # release mode compilation fix for unused variables
