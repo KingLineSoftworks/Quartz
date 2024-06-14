c8e0dd6b00b751a25e6fc16a7a5e86133f824a44
Kejoko <keeganjkochis@gmail.com>
2024-06-14 13:43:40 -0600

    Clean up the way we have compile definitions and include directories so as to not pollute vendor source

---
+++ demo_app/jolt_testing/Loggers.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "util/logger/Logger.hpp"
+
+DECLARE_LOGGER(GENERAL, trace);
+DECLARE_LOGGER(BIGBOY, trace);
+DECLARE_LOGGER(ALAMANCY, trace);
+DECLARE_LOGGER(GENERAL2, trace);
+
+DECLARE_LOGGER_GROUP(
+    DEMO_APP,
+    4,
+    GENERAL,
+    BIGBOY,
+    ALAMANCY,
+    GENERAL2
+);
