16ba616fef5b5cde3f981e77424017083c623d0e
Kejoko <keeganjkochis@gmail.com>
2024-10-05 17:29:11 -0600

    Safely generate compile commands json file

---
+++ PreLoad.cmake
@@ -0,0 +1,11 @@
+#====================================================================
+# CMake information
+# This file should be preprocessed before the main CMakeLists.txt
+#====================================================================
+
+set(CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
+
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang")
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "" FORCE)
