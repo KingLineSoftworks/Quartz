ba99eff86f494ad09fcbb63e72cd45a5a81aab84
Kejoko <keeganjkochis@gmail.com>
2024-10-25 17:22:03 -0600

    Add uncrustify configuration

---
+++ style/uncrustify/class_header_template.hpp
@@ -0,0 +1,6 @@
+/**
+ * @class $(class)
+ * @brief 
+ * 
+ * @details
+ */
---
+++ style/uncrustify/file_header_template.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file $(filename)
+ * @author yourname (your@email.com)
+ * @brief 
+ * @version quartz
+ * @date $(year)-__-__
+ * 
+ * @copyright Copyright (c) $(year), KingLine Softworks
+ *
+ * Dependencies:
+ *   -
+ * 
+ * Known Issues:
+ *   -
+ */
+
