acd5f2b620a6a9f11385ddd89cb201a693a16c8b
Kejoko <keeganjkochis@gmail.com>
2025-07-11 10:52:53 -0600

    Put unit test executables in the bin/ut directory, so they are not next to important executables

--- cmake/CreateUnitTest.cmake
+++ cmake/CreateUnitTest.cmake
@@ -10,6 +10,7 @@ function(create_unit_test unit_test_name unit_test_main_file unit_test_link_libr
     target_compile_options(${unit_test_name} PRIVATE ${QUARTZ_CMAKE_CXX_FLAGS})
     target_include_directories(${unit_test_name} PRIVATE ${QUARTZ_UNIT_TEST_INCLUDE_DIRS} )
     target_link_libraries(${unit_test_name} PRIVATE ${unit_test_link_libraries} UTIL_UnitTest)
+    set_target_properties(${unit_test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/ut)
     add_test(NAME ${unit_test_name} COMMAND ${unit_test_name})
 endfunction()
 
