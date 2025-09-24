534184027ba330a644fee9e63eb90488783d412a
Kejoko <keeganjkochis@gmail.com>
2025-09-24 10:58:06 -0600

    Unit test for validating UBO info stride calc (using vulkan spec power of 2 alignment)

--- test/unit/quartz/rendering/pipeline/test_UniformBufferInfo.cpp
+++ test/unit/quartz/rendering/pipeline/test_UniformBufferInfo.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include <optional>
 
+#include "util/logger/Logger.hpp"
 #include "util/unit_test/UnitTest.hpp"
 
 #include "quartz/rendering/pipeline/UniformBufferInfo.hpp"
@@ -12,18 +14,39 @@ uint32_t manualStride(
         return uboSizeBytes;
     }
 
-    // TODO - what the fuck is our stride function actually doing? I don't get it ...
-    return minAlignment + uboSizeBytes;
+    // Take our uboSize and round up to the next multiple of our alignment size
+
+    const double fitment = static_cast<double>(uboSizeBytes) / static_cast<double>(minAlignment);
+    const uint32_t alignmentMultiples = std::ceil(fitment);
+    return alignmentMultiples * minAlignment;
 }
 
+/**
+ * @brief From the vulkan spec:
+ *    """
+ *    `minUniformBufferOffsetAlignment` is the minimum __required__ alignment, in bytes, for the
+ *    `offset` member of the `VkDescriptorBufferInfo` structure for uniform buffers.
+ *    When a descriptor of type `VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER` or `VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC`
+ *    is updated, the `offset` __must__ be an integer multiple of this limit.
+ *    Similarly, dynamic offsets for uniform buffers __must__ be multiples of this limit.
+ *    The value __must__ be a power of two.
+ *    """
+ */
 UT_FUNCTION(test_calculateDynamicUniformBufferByteStride) {
     struct TestInfo {
         uint32_t minAlignment;
         uint32_t uboSizeBytes;
     };
 
+    // Create the list of testing information
+    // We do this by creating a list of alignments (each of which is a power of 2),
+    //    then creating a set of uboSizes to test each alignment
     std::vector<TestInfo> testInfos;
-    const std::vector<uint32_t> alignments = { 0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 23, 37, 50, 199, 55555, 6666 };
+    std::vector<uint32_t> alignments;
+    const uint32_t maxPower = 8;
+    for (uint32_t power = 0; power <= maxPower; ++power) {
+        alignments.push_back(std::pow(2, power));
+    }
     const double iterations = 10;
     for (const uint32_t alignment : alignments) {
         for (uint32_t uboSize = 1; uboSize < (alignment * iterations + 1); uboSize++) {
@@ -32,7 +55,7 @@ UT_FUNCTION(test_calculateDynamicUniformBufferByteStride) {
     }
 
     for (const TestInfo& testInfo : testInfos) {
-        UT_CHECK_NOT_EQUAL(
+        UT_CHECK_EQUAL(
             quartz::rendering::UniformBufferInfo::calculateDynamicUniformBufferByteStride(
                 testInfo.minAlignment,
                 testInfo.uboSizeBytes
